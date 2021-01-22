e64371217cd4578b4dfe8a360590119da56d7638
webnickell <50118217+webnickell@users.noreply.github.com>
2021-01-22 17:17:19 +0200

    add pages

---
+++ lib/ui/pages/map_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatefulWidget {
+  MapPage({Key key}) : super(key: key);
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test task for INT20H'),
+      ),
+      body: Container(),
+    );
+  }
+}
